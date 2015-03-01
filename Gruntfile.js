d4aa021947839597edf482091e7bd51f00c20e3f
Mike Calvanese <mfcalvanese@gmail.com>
2015-03-01 17:00:10 -0500

    Switch default browser to Chrome

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
                 configFile: './karma.conf.js',
                 autoWatch: false,
                 singleRun: true,
-                browsers: ['PhantomJS']
+                browsers: ['Chrome']
             }
         },
 
