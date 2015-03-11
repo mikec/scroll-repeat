0d321bc351d34f078fec2d035b92a5f6ee03c37f
Mike Calvanese <mfcalvanese@gmail.com>
2015-03-11 12:20:36 -0400

    Add loading indicator
    shown while the json data is being requested

--- demo/grid/grid.js
+++ demo/grid/grid.js
@@ -6,12 +6,15 @@ function($http, $rootScope) {
 
     var $this = this;
 
+    this.loading = true;
+
     $http.get('flights.json')
         .then(function(resp) {
             for(var i=0; i < resp.data.length; i++) {
                 resp.data[i].record_number = (i + 1);
             }
             $this.flights = resp.data;
+            $this.loading = false;
         });
 
 }]);
