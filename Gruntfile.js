eb19a806d97a80e0f383df9070e0698ce513c070
Mike Calvanese <mfcalvanese@gmail.com>
2015-03-12 10:31:08 -0500

    Build version 0.0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 
 module.exports = function(grunt) {
 
+    var shell = require('shelljs');
+
     var gruntConfig = {
         pkg: grunt.file.readJSON('package.json'),
 
@@ -95,10 +97,64 @@ module.exports = function(grunt) {
         'package'
     ]);
 
+    grunt.registerTask('release-prepare', 'Set up submodule to receive a new release',
+    function() {
+        // Make sure we have the submodule in dist
+        run("git submodule init");
+        run("git submodule update");
+        run("cd dist; git checkout master");
+        // Bump version
+        var newVer = grunt.config('pkg').version;
+        //var comp = grunt.file.readJSON("dist/bower.json");
+        //grunt.log.writeln("Package version: " + newVer);
+        //grunt.log.writeln("Component version: " + comp.version);
+        //if( !semver.gt( newVer, comp.version ) ){
+        //    grunt.warn("Need to up-version package.json first!");
+        //}
+    });
+
+
+    grunt.registerTask('release-commit', 'push new build to bower component repo',
+    function() {
+        // Stamp version
+        var newVer = grunt.config('pkg').version;
+        //var comp = grunt.file.readJSON("dist/bower.json");
+        grunt.log.writeln("Package version: " + newVer);
+        //grunt.log.writeln("Component version: " + comp.version);
+        //if( !semver.gt( newVer, comp.version ) ){
+        //    grunt.warn("Need to up-version package.json first!");
+        //}
+        //comp.version = newVer;
+        //grunt.file.write("dist/bower.json", JSON.stringify(comp, null, '  ')+'\n');
+        // Commit submodule
+        // Tag submodule
+        run('cd dist; git commit -a -m"Build version '+ newVer +'"', "Commited to bower repo");
+        run('cd dist; git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged bower repo");
+        // Commit and tag this.
+        run('git commit -a -m"Build version '+ newVer +'"', "Commited to source repo");
+        run('git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged source repo");
+        run("git submodule update");
+        // push?
+        grunt.log.ok("DON'T FORGET TO PUSH BOTH!");
+    });
+
+    grunt.registerTask('release', [
+        'release-prepare',
+        'package',
+        'release-commit'
+    ]);
+
     grunt.registerTask('default', [
         'build',
         'connect',
         'watch'
     ]);
+
+    function run(cmd, msg){
+        shell.exec(cmd, {silent:true});
+        if( msg ){
+            grunt.log.ok(msg);
+        }
+    }
 
 };
